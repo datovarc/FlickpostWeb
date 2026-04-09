c3b0da464c9323b225f76e476cc1131374a4e9b2
David <david.tovarc91@gmail.com>
2026-04-09 18:17:12 +0800

    feat: update add status API payload

--- src/main/java/co/flickpost/admin/models/json/AddStatusSaveRequest.java
+++ src/main/java/co/flickpost/admin/models/json/AddStatusSaveRequest.java
@@ -9,5 +9,9 @@ import java.util.List;
 @Setter
 public class AddStatusSaveRequest {
     private String dateTime;
+    private String location;
+    private String shipmentMainStatus;
+    private String shipmentParcelStatus;
+    private String note;
     private List<AuditedValuesByTrackingPayload> packages;
 }
